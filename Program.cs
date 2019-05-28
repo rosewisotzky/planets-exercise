019eeff0a9cda85257c061a78c2c512b2c3c83ba
Rose Wisotzky <rose@rosewisotzky.com>
2019-05-28 11:16:27 -0500

    first commit and planet list

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace planets_spaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wow, a new week, pal! Look at you learning all the learnin'");
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+        }
+    }
+}
